89dea17de072f50510c72c0faec3ce79324c19b6
brunettisergio <87024293+brunettisergio@users.noreply.github.com>
2025-06-30 18:44:55 -0400

    Changed navigation-bar component to site-header component.

---
+++ src/app/site-header/site-header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-site-header',
+    standalone: true,
+    imports: [],
+    templateUrl: './site-header.html',
+    styleUrl: './site-header.scss',
+})
+export class SiteHeader {}
