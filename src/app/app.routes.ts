b942dd7335b121d2323545d6ce5da018d2b7ffc2
brunettisergio <87024293+brunettisergio@users.noreply.github.com>
2025-06-30 21:08:12 -0400

    Fixes routing bug.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
+import { ExperiencesComponent } from './experiences/experiences';
 import { LodgingComponent } from './lodging/lodging';
 import { FoodAndDrinksComponent } from './food-and-drinks/food-and-drinks';
 import { TransportationComponent } from './transportation/transportation';
@@ -8,6 +9,7 @@ import { FaqComponent } from './faq/faq';
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'experiences', component: ExperiencesComponent },
     { path: 'lodging', component: LodgingComponent },
     { path: 'food-and-drinks', component: FoodAndDrinksComponent },
     { path: 'transportation', component: TransportationComponent },
