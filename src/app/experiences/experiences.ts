821c77968382b0cf50f067a9ac1fa073f081dbee
brunettisergio <87024293+brunettisergio@users.noreply.github.com>
2025-07-01 19:38:54 -0400

    Adds content and styling to Experiences page.

--- src/app/experiences/experiences.ts
+++ src/app/experiences/experiences.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-experiences',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './experiences.html',
-    styleUrl: './experiences.scss',
+    styleUrls: ['./experiences.scss'],
 })
-export class ExperiencesComponent {}
+export class ExperiencesComponent {
+    currentSlide = 0;
+
+    slides = [
+        'Boat Charters',
+        'Snorkeling Adventures',
+        'Golf Course',
+        'Helicopter Rides',
+        'Volcano Sightseeing',
+        'Rainforest Zipline',
+    ];
+
+    nextSlide(): void {
+        this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+    }
+
+    previousSlide(): void {
+        this.currentSlide =
+            (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    }
+
+    goToSlide(index: number): void {
+        this.currentSlide = index;
+    }
+}
